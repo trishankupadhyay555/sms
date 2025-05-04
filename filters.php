b6e33967d65d04efc48f6af21383d384dc416f10
trishankupadhyay555 <107575553+trishankupadhyay555@users.noreply.github.com>
2025-05-04 08:29:31 -0500

    Update filters.php

--- filters.php
+++ filters.php
@@ -1,1 +1,17 @@
+<?php
+header('Content-Type: application/json');
 
+// Path to the filters file on the server
+$filtersFile = 'sms_filters.json';
+
+// Check if the file exists
+if (file_exists($filtersFile)) {
+    // Read the file content
+    $filtersContent = file_get_contents($filtersFile);
+    echo $filtersContent;
+} else {
+    // Return an error if the file doesn't exist
+    http_response_code(404);
+    echo json_encode(array("error" => "Filters file not found"));
+}
+?>
